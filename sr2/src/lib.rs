61e34b022cb9bfc861113ca4c379edf1f646c3d2
Paweł Zatoka <github@hoxi.me>
2026-07-18 06:42:42 +0200

    feat: implement helper crate to sr2

--- sr2/src/lib.rs
+++ sr2/src/lib.rs
@@ -102,7 +102,6 @@ struct Settings {
 
 async fn main() {
     let mut settings = Settings::register();
-    let mut watchers = Watchers::new();
 
     loop {
         let process = Process::wait_attach(PROCESS_NAME).await;
@@ -110,13 +109,15 @@ async fn main() {
             .until_closes(async {
                 if let Ok(base_address) = process.get_module_address(PROCESS_NAME) {
                     if let Some(version) = Version::detect(&process, base_address) {
+                        let mut watchers = Watchers::new(version);
+
                         loop {
                             settings.update();
-                            watchers.update(&process, base_address, version);
+                            watchers.update(&process, base_address);
                             let timer_state = timer::state();
 
                             if let (Some(save_load), Some(cutscene_load)) =
-                                (&watchers.save_load.pair, &watchers.cutscene_load.pair)
+                                (watchers.save_load.pair(), watchers.cutscene_load.pair())
                             {
                                 if cutscene_load.current == 0 || save_load.current == 0 {
                                     timer::pause_game_time();
@@ -125,20 +126,16 @@ async fn main() {
                                 }
                             }
 
-                            if timer_state == TimerState::NotRunning {
-                                if settings.timer_start {
-                                    if let (Some(cutscene), Some(start_flag)) =
-                                        (&watchers.cutscene.pair, &watchers.start_flag.pair)
-                                    {
-                                        if let Ok(current_cutscene) =
-                                            cutscene.current.validate_utf8()
+                            if timer_state == TimerState::NotRunning && settings.timer_start {
+                                if let (Some(cutscene), Some(start_flag)) =
+                                    (watchers.cutscene.pair(), watchers.start_flag.pair())
+                                {
+                                    if let Ok(current_cutscene) = cutscene.current.validate_utf8() {
+                                        if current_cutscene == "TSSP01-01.cscx"
+                                            && start_flag.current == 1
+                                            && start_flag.old != start_flag.current
                                         {
-                                            if current_cutscene == "TSSP01-01.cscx"
-                                                && start_flag.current == 1
-                                                && start_flag.old != start_flag.current
-                                            {
-                                                timer::start();
-                                            }
+                                            timer::start();
                                         }
                                     }
                                 }
@@ -146,7 +143,7 @@ async fn main() {
 
                             if timer_state == TimerState::Running {
                                 if settings.timer_reset {
-                                    if let Some(cutscene) = &watchers.cutscene.pair {
+                                    if let Some(cutscene) = watchers.cutscene.pair() {
                                         if cutscene.changed() {
                                             if let Ok(current_cutscene) =
                                                 cutscene.current.validate_utf8()
@@ -160,7 +157,7 @@ async fn main() {
                                 }
 
                                 if settings.hundo {
-                                    if let Some(progress_percent) = &watchers.progress_percent.pair
+                                    if let Some(progress_percent) = watchers.progress_percent.pair()
                                     {
                                         if progress_percent.current == 100
                                             && progress_percent.changed()
@@ -170,171 +167,33 @@ async fn main() {
                                     }
                                 }
 
-                                if settings.missions {
-                                    if let Some(missions) = &watchers.missions.pair {
-                                        if missions.current == missions.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
+                                let counter_settings = [
+                                    (settings.missions, "missions"),
+                                    (settings.strongholds, "strongholds"),
+                                    (settings.tags, "tags"),
+                                    (settings.cd, "cd"),
+                                    (settings.jumps, "jumps"),
+                                    (settings.barnstorming, "barnstorming"),
+                                    (settings.chop_shop, "chop_shop"),
+                                    (settings.crowd_control, "crowd_control"),
+                                    (settings.derby, "derby"),
+                                    (settings.escort, "escort"),
+                                    (settings.fight_club, "fight_club"),
+                                    (settings.fuzz, "fuzz"),
+                                    (settings.heli_assault, "heli_assault"),
+                                    (settings.hitman, "hitman"),
+                                    (settings.fraud, "fraud"),
+                                    (settings.mayhem, "mayhem"),
+                                    (settings.races, "races"),
+                                    (settings.septic, "septic"),
+                                    (settings.snatch, "snatch"),
+                                    (settings.trafficking, "trafficking"),
+                                    (settings.trail_blazing, "trail_blazing"),
+                                ];
 
-                                if settings.strongholds {
-                                    if let Some(strongholds) = &watchers.strongholds.pair {
-                                        if strongholds.current == strongholds.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.tags {
-                                    if let Some(tags) = &watchers.tags.pair {
-                                        if tags.current == tags.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.cd {
-                                    if let Some(cd) = &watchers.cd.pair {
-                                        if cd.current == cd.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.jumps {
-                                    if let Some(jumps) = &watchers.jumps.pair {
-                                        if jumps.current == jumps.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.barnstorming {
-                                    if let Some(barnstorming) = &watchers.barnstorming.pair {
-                                        if barnstorming.current == barnstorming.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.chop_shop {
-                                    if let Some(chop_shop) = &watchers.chop_shop.pair {
-                                        if chop_shop.current == chop_shop.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.crowd_control {
-                                    if let Some(crowd_control) = &watchers.crowd_control.pair {
-                                        if crowd_control.current == crowd_control.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.derby {
-                                    if let Some(derby) = &watchers.derby.pair {
-                                        if derby.current == derby.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.escort {
-                                    if let Some(escort) = &watchers.escort.pair {
-                                        if escort.current == escort.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.fight_club {
-                                    if let Some(fight_club) = &watchers.fight_club.pair {
-                                        if fight_club.current == fight_club.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.fuzz {
-                                    if let Some(fuzz) = &watchers.fuzz.pair {
-                                        if fuzz.current == fuzz.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.heli_assault {
-                                    if let Some(heli_assault) = &watchers.heli_assault.pair {
-                                        if heli_assault.current == heli_assault.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.hitman {
-                                    if let Some(hitman) = &watchers.hitman.pair {
-                                        if hitman.current == hitman.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.fraud {
-                                    if let Some(fraud) = &watchers.fraud.pair {
-                                        if fraud.current == fraud.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.mayhem {
-                                    if let Some(mayhem) = &watchers.mayhem.pair {
-                                        if mayhem.current == mayhem.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.races {
-                                    if let Some(races) = &watchers.races.pair {
-                                        if races.current == races.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.septic {
-                                    if let Some(septic) = &watchers.septic.pair {
-                                        if septic.current == septic.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.snatch {
-                                    if let Some(snatch) = &watchers.snatch.pair {
-                                        if snatch.current == snatch.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.trafficking {
-                                    if let Some(trafficking) = &watchers.trafficking.pair {
-                                        if trafficking.current == trafficking.old + 1 {
-                                            timer::split();
-                                        }
-                                    }
-                                }
-
-                                if settings.trail_blazing {
-                                    if let Some(trail_blazing) = &watchers.trail_blazing.pair {
-                                        if trail_blazing.current == trail_blazing.old + 1 {
-                                            timer::split();
-                                        }
+                                for (enabled, name) in counter_settings {
+                                    if enabled && watchers.counters[name].increased_by(1) {
+                                        timer::split();
                                     }
                                 }
                             }
--- sr2/src/watchers.rs
+++ sr2/src/watchers.rs
@@ -1,136 +1,64 @@
-use asr::{string::ArrayCString, watcher::Watcher, Address, Process};
+use asr::{string::ArrayCString, Address, Process};
+use autosplitter_helpers::{MemoryWatcher, MemoryWatcherMap};
 
 use crate::version::Version;
 
 pub struct Watchers {
-    pub start_flag: Watcher<u32>,
-    pub progress_percent: Watcher<u32>,
-    pub cutscene_load: Watcher<u32>,
-    pub cutscene: Watcher<ArrayCString<255>>,
-    pub save_load: Watcher<u8>,
-    pub missions: Watcher<u32>,
-    pub strongholds: Watcher<u32>,
-    pub tags: Watcher<u32>,
-    pub cd: Watcher<u32>,
-    pub jumps: Watcher<u32>,
-    pub barnstorming: Watcher<u32>,
-    pub chop_shop: Watcher<u32>,
-    pub crowd_control: Watcher<u32>,
-    pub derby: Watcher<u32>,
-    pub escort: Watcher<u32>,
-    pub fight_club: Watcher<u32>,
-    pub fuzz: Watcher<u32>,
-    pub heli_assault: Watcher<u32>,
-    pub hitman: Watcher<u32>,
-    pub fraud: Watcher<u32>,
-    pub mayhem: Watcher<u32>,
-    pub races: Watcher<u32>,
-    pub septic: Watcher<u32>,
-    pub snatch: Watcher<u32>,
-    pub trafficking: Watcher<u32>,
-    pub trail_blazing: Watcher<u32>,
-}
-
-impl Default for Watchers {
-    fn default() -> Self {
-        Self::new()
-    }
+    pub start_flag: MemoryWatcher<u32>,
+    pub progress_percent: MemoryWatcher<u32>,
+    pub cutscene_load: MemoryWatcher<u32>,
+    pub cutscene: MemoryWatcher<ArrayCString<255>>,
+    pub save_load: MemoryWatcher<u8>,
+    pub counters: MemoryWatcherMap<u32>,
 }
 
 impl Watchers {
-    pub fn new() -> Self {
-        Self {
-            start_flag: Watcher::new(),
-            progress_percent: Watcher::new(),
-            cutscene_load: Watcher::new(),
-            cutscene: Watcher::new(),
-            save_load: Watcher::new(),
-            missions: Watcher::new(),
-            strongholds: Watcher::new(),
-            tags: Watcher::new(),
-            cd: Watcher::new(),
-            jumps: Watcher::new(),
-            barnstorming: Watcher::new(),
-            chop_shop: Watcher::new(),
-            crowd_control: Watcher::new(),
-            derby: Watcher::new(),
-            escort: Watcher::new(),
-            fight_club: Watcher::new(),
-            fuzz: Watcher::new(),
-            heli_assault: Watcher::new(),
-            hitman: Watcher::new(),
-            fraud: Watcher::new(),
-            mayhem: Watcher::new(),
-            races: Watcher::new(),
-            septic: Watcher::new(),
-            snatch: Watcher::new(),
-            trafficking: Watcher::new(),
-            trail_blazing: Watcher::new(),
-        }
-    }
-
-    pub fn update(&mut self, process: &Process, base: Address, version: Version) {
-        let base_address = base.value();
-        let start_flag_addr: u64 = if version == Version::GOG {
+    pub fn new(version: Version) -> Self {
+        let start_flag = if version == Version::GOG {
             0x1F870A0
         } else {
             0x1F870C0
         };
 
-        self.start_flag
-            .update(process.read(base_address + start_flag_addr).ok());
-        self.progress_percent
-            .update(process.read(base_address + 0x1052C58).ok());
-        self.cutscene_load
-            .update(process.read(base_address + 0xA9D670).ok());
-        self.cutscene.update(
-            process
-                .read_pointer_path(base, asr::PointerSize::Bit32, &[0x02127D10, 0x4, 0x0])
-                .ok(),
-        );
-        self.save_load
-            .update(process.read(base_address + 0xA8EB88).ok());
+        let mut counters = MemoryWatcherMap::new();
+        counters.insert("missions", 0x1053384);
+        counters.insert("strongholds", 0x10533C8);
+        counters.insert("tags", 0x10535E8);
+        counters.insert("cd", 0x27C7150);
+        counters.insert("jumps", 0x10535A4);
+        counters.insert("barnstorming", 0x1053670);
+        counters.insert("chop_shop", 0x10536B4);
+        counters.insert("crowd_control", 0x10537C4);
+        counters.insert("derby", 0x1053890);
+        counters.insert("escort", 0x10539A0);
+        counters.insert("fight_club", 0x1053A28);
+        counters.insert("fuzz", 0x1053AB0);
+        counters.insert("heli_assault", 0x1053B38);
+        counters.insert("hitman", 0x10536F8);
+        counters.insert("fraud", 0x1053D14);
+        counters.insert("mayhem", 0x1053E68);
+        counters.insert("races", 0x1055760);
+        counters.insert("septic", 0x1053F34);
+        counters.insert("snatch", 0x1054000);
+        counters.insert("trafficking", 0x1053918);
+        counters.insert("trail_blazing", 0x1053C04);
 
-        self.missions
-            .update(process.read(base_address + 0x1053384).ok());
-        self.strongholds
-            .update(process.read(base_address + 0x10533C8).ok());
-        self.tags
-            .update(process.read(base_address + 0x10535E8).ok());
-        self.cd.update(process.read(base_address + 0x27C7150).ok());
-        self.jumps
-            .update(process.read(base_address + 0x10535A4).ok());
-        self.barnstorming
-            .update(process.read(base_address + 0x1053670).ok());
-        self.chop_shop
-            .update(process.read(base_address + 0x10536B4).ok());
-        self.crowd_control
-            .update(process.read(base_address + 0x10537C4).ok());
-        self.derby
-            .update(process.read(base_address + 0x1053890).ok());
-        self.escort
-            .update(process.read(base_address + 0x10539A0).ok());
-        self.fight_club
-            .update(process.read(base_address + 0x1053A28).ok());
-        self.fuzz
-            .update(process.read(base_address + 0x1053AB0).ok());
-        self.heli_assault
-            .update(process.read(base_address + 0x1053B38).ok());
-        self.hitman
-            .update(process.read(base_address + 0x10536F8).ok());
-        self.fraud
-            .update(process.read(base_address + 0x1053D14).ok());
-        self.mayhem
-            .update(process.read(base_address + 0x1053E68).ok());
-        self.races
-            .update(process.read(base_address + 0x1055760).ok());
-        self.septic
-            .update(process.read(base_address + 0x1053F34).ok());
-        self.snatch
-            .update(process.read(base_address + 0x1054000).ok());
-        self.trafficking
-            .update(process.read(base_address + 0x1053918).ok());
-        self.trail_blazing
-            .update(process.read(base_address + 0x1053C04).ok());
+        Self {
+            start_flag: MemoryWatcher::new(start_flag),
+            progress_percent: MemoryWatcher::new(0x1052C58),
+            cutscene_load: MemoryWatcher::new(0xA9D670),
+            cutscene: MemoryWatcher::new([0x02127D10, 0x4, 0x0]),
+            save_load: MemoryWatcher::new(0xA8EB88),
+            counters,
+        }
+    }
+
+    pub fn update(&mut self, process: &Process, base: Address) {
+        self.start_flag.update(process, base);
+        self.progress_percent.update(process, base);
+        self.cutscene_load.update(process, base);
+        self.cutscene.update(process, base);
+        self.save_load.update(process, base);
+        self.counters.update_all(process, base);
     }
 }
